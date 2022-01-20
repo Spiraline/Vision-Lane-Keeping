c83b022438c052ea5aca9d6b8d87e167e048f8e4
Spiraline <spiraline@naver.com>
2022-01-20 20:17:03 +0900

    Add vehicle_cmd_publisher

---
+++ vehicle_cmd_publisher.py
@@ -0,0 +1,17 @@
+import rospy
+from autoware_msgs.msg import VehicleCmd
+from geometry_msgs.msg import TwistStamped
+
+def twist_cb(data):
+    msg = VehicleCmd()
+    msg.twist_cmd = data
+    vehicle_pub.publish(msg)
+
+if __name__ == '__main__':
+    rospy.init_node('vehicle_cmd_publihser')
+
+    vehicle_pub = rospy.Publisher("vehicle_cmd_lgsvl", VehicleCmd, queue_size = 10)
+
+    rospy.Subscriber("/twist_cmd", TwistStamped, twist_cb)
+
+    rospy.spin()
